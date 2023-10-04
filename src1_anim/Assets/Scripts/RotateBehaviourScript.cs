070cbb1d2741c2513934303d97a6b3d4153d282f
ZakiAKR <zakichan.ak@gmail.com>
2023-10-04 22:16:17 +0900

    20231004

--- src1_anim/Assets/Scripts/RotateBehaviourScript.cs
+++ src1_anim/Assets/Scripts/RotateBehaviourScript.cs
@@ -6,5 +6,9 @@ public class RotateBehaviourScript : MonoBehaviour
 {
     void Update()
     {
+        // 360‚Åˆêü(360.0f)@‚P•b‚Å‚P‘‚¦‚é(Time.deltaTime)@ˆêü‚Ì•b”(10.0f)
+        float rotate = 360.0f * Time.deltaTime / 10.0f;
+
+        this.transform.Rotate(0, rotate, 0);
     }
 }
